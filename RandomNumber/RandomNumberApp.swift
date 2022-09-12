6d0ef9b57353b63288268a10bb7f8cb6314eaf02
Aleksandar Drakaliyski <theall3xander@gmail.com>
2022-09-12 18:37:17 +0100

    Initial Commit

---
+++ RandomNumber/RandomNumberApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomNumberApp.swift
+//  RandomNumber
+//
+//  Created by Aleksandar Drakaliyski on 12/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct RandomNumberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
